ea2ef7941cd561643e9adc1366b0f14e98cbbd1e
UDAY <112105850+UdayChandran95@users.noreply.github.com>
2024-08-15 23:12:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import HomePage from "./Homepage/HomePage";
+import About from "./About/About";
+import Services from "./Services/Services";
+import ServicePage from "./Services/ServicePage";
+import ServicePage2 from "./Services/ServicePage2";
+import ServicePage3 from "./Services/ServicePage3";
+import Blog1 from "./Blog/Blog1";
+import Blog2 from "./Blog/Blog2";
+import Blog3 from "./Blog/Blog3";
+import Gallery from "./Gallery/Gallery";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/servicepage" element={<ServicePage />} />
+      <Route path="/servicepage2" element={<ServicePage2 />} />
+      <Route path="/servicepage3" element={<ServicePage3 />} />
+      <Route path="/blog1" element={<Blog1 />} />
+      <Route path="/blog2" element={<Blog2 />} />
+      <Route path="/blog3" element={<Blog3 />} />
+      <Route path="/gallery" element={<Gallery />} />
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
